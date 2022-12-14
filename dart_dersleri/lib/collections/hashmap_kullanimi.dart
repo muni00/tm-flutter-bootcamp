b9f8946c71b7518e66468bdc3f1fce3acc2c5211
Muazzez Nihal Bahadır <58309495+muni00@users.noreply.github.com>
2022-12-14 11:17:45 +0300

    dördüncü ders bitti
    dart konuları bitti bunu devamı olarak aynı repo içinde flutter dersleri için de uygulamalar devam edecektir

---
+++ dart_dersleri/lib/collections/hashmap_kullanimi.dart
@@ -0,0 +1,36 @@
+import 'dart:collection';
+
+void main() {
+  //Tanımlama
+  var sayilar = {"Bir": 1, "İki": 2};
+  var iller = HashMap<int, String>();
+
+  //Veri ekleme
+  iller[16] = "Bursa";
+  iller[34] = "İstanbul";
+  print(iller);
+
+  //Güncelleme
+
+  iller[16] = "Yeni Bursa";
+  print(iller);
+
+  String il = iller[34]!;
+  print(il);
+
+  print(iller.length);
+  print(iller.isEmpty);
+  print(iller.containsKey(16));
+  print(iller.containsValue("Nursa"));
+
+  var anahtarlar = iller.keys;
+  for (var a in anahtarlar) {
+    print("$a -> ${iller[a]}");
+  }
+
+  iller.remove(16);
+  print(iller);
+
+  iller.clear();
+  print(iller);
+}
